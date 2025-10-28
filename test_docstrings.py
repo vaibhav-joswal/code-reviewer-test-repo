9c0866308e15af76b374db6d13821178c8023b86
VAIBHAV JOSWAL <vaibhawjoswal2906@gmail.com>
2025-10-28 20:33:37 +0530

    Add comment to my_function for clarity
    Added a comment to clarify the return statement.

--- test_docstrings.py
+++ test_docstrings.py
@@ -1,6 +1,6 @@
 def my_function(a, b):
     # This is a normal comment, not a docstring.
-    return a + b
+    return a + b #I am adding this comment to understand the working of pull request
 
 class MyClass:
     def another_function(self):
